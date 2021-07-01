195f018e63d57166d407439292c84d22d80b512d
minmyatthu <minmyatthu2018.mlm@gmail.com>
2021-07-01 20:24:03 +0630

    fix typo

--- courseinfo/tailwind.config.js
+++ courseinfo/tailwind.config.js
@@ -1,7 +1,7 @@
 const colors = require('tailwindcss/colors');
 module.exports = {
   mode: 'jit',
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
