855e1b5a718810e5bf93036b55691173b1206bbb
minmyatthu <minmyatthu2018.mlm@gmail.com>
2021-07-01 20:22:44 +0630

    fix typo

--- courseinfo/src/Components/Total.jsx
+++ courseinfo/src/Components/Total.jsx
@@ -4,7 +4,7 @@ const Total = ({ course }) => {
   const exercises = course.parts.map((part) => part.exercises);
   const sum = exercises.reduce((accumulator, current) => accumulator + current);
 
-  return <p className="sm:text-xl font-bold">Totall of {sum} exercises</p>;
+  return <p className="sm:text-xl font-bold">Total of {sum} exercises</p>;
 };
 
 export default Total;
