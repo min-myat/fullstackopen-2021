4b95f32e9762e34ad0adeb624bba0752155fde8e
minmyatthu <minmyatthu2018.mlm@gmail.com>
2021-07-01 21:28:28 +0630

    exercise 2.6

---
+++ phonebook/src/App.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import ContactForm from './components/ContactForm';
+import ContactInfo from './components/ContactInfo';
+import Header from './components/Header';
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
+
+  return (
+    <div className="flex flex-col items-center">
+      <Header />
+      <div className="mt-10 p-5 bg-white max-w-lg rounded shadow-md">
+        <ContactForm persons={persons} setPersons={setPersons} />
+        <hr className="my-10" />
+        <ContactInfo persons={persons} />
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ phonebook/tailwind.config.js
@@ -0,0 +1,18 @@
+const colors = require('tailwindcss/colors');
+module.exports = {
+  purge: ['./index.html', './src/**/*.{js,jsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        sky: colors.sky,
+      },
+    },
+  },
+  variants: {
+    extend: {
+      backgroundColor: ['active'],
+    },
+  },
+  plugins: [require('@tailwindcss/forms')],
+};
