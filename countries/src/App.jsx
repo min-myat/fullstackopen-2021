9f158ddd53d24154fefe4ae2da3683b4ca4e34ad
minmyatthu <minmyatthu2018.mlm@gmail.com>
2021-07-02 13:42:09 +0630

    exercise 2.12

---
+++ countries/src/App.jsx
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+import FindCountry from './components/FindCountry';
+
+function App() {
+  const [countries, setCountries] = useState();
+
+  useEffect(() => {
+    const url = 'https://restcountries.eu/rest/v2/all';
+    axios.get(url).then(({ data }) => setCountries(data));
+  }, []);
+
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <div className="py-4 w-full bg-green-400 text-center shadow mb-6">
+        <h1 className="text-3xl text-white font-bold uppercase">Countries</h1>
+      </div>
+
+      {countries && (
+        <div className="w-1/2 bg-white rounded shadow-md p-6">
+          <FindCountry countries={countries} />
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
---
+++ countries/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [
+    './index.html',
+    './src/**/*.{js, jsx, ts, tsx}',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
+}
